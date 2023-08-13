b0e369972151d27b019506465a1e84f515e0790c
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2023-08-13 16:20:49 +0300

    W-2 Setup JWT service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,190 +1,163 @@
-import { Prisma, PrismaClient } from '@prisma/client'
-import fastify from 'fastify'
+import fastify from "fastify";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { PrismaClient, User } from "@prisma/client";
 
-const prisma = new PrismaClient()
-const app = fastify({ logger: true })
+declare module "fastify" {
+  interface FastifyRequest {
+    user: User | null;
+  }
+}
 
-app.post<{
-  Body: ISignupBody
-}>(`/signup`, async (req, res) => {
-  const { name, email, posts } = req.body
+const SECRET_KEY = process.env.SECRET_KEY;
+const PORT = process.env.PORT;
 
-  const postData = posts?.map((post: Prisma.PostCreateInput) => {
-    return { title: post?.title, content: post?.content }
-  })
+if (!SECRET_KEY || !PORT) {
+  throw new Error("Provide a SECRET_KEY and PORT env variables");
+}
 
-  const result = await prisma.user.create({
-    data: {
-      name,
-      email,
-      posts: {
-        create: postData,
-      },
-    },
-  })
-  return result
-})
+const prisma = new PrismaClient();
+const app = fastify({ logger: true }).decorate("user", null);
 
-app.post<{
-  Body: ICreatePostBody
-}>(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      author: { connect: { email: authorEmail } },
-    },
-  })
-  return result
-})
+app.addHook("preHandler", async (req, reply) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
 
-app.put<{
-  Params: IPostByIdParam
-}>('/post/:id/views', async (req, res) => {
-  const { id } = req.params
+  if (!token) {
+    return;
+  }
 
   try {
-    const post = await prisma.post.update({
-      where: { id: Number(id) },
-      data: {
-        viewCount: {
-          increment: 1,
-        },
-      },
-    })
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const validated = decoded as { userId: string };
 
-    return post
-  } catch (error) {
-    return { error: `Post with ID ${id} does not exist in the database` }
-  }
-})
+    const user = await prisma.user.findUnique({
+      where: { publicId: validated.userId },
+    });
 
-app.put<{
-  Params: IPostByIdParam
-}>('/publish/:id', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    const postData = await prisma.post.findUnique({
-      where: { id: Number(id) },
-      select: {
-        published: true,
-      },
-    })
-
-    const updatedPost = await prisma.post.update({
-      where: { id: Number(id) || undefined },
-      data: { published: !postData?.published },
-    })
-    return updatedPost
-  } catch (error) {
-    return { error: `Post with ID ${id} does not exist in the database` }
-  }
-})
-
-app.delete<{
-  Params: IPostByIdParam
-}>(`/post/:id`, async (req, res) => {
-  const { id } = req.params
-  const post = await prisma.post.delete({
-    where: {
-      id: Number(id),
-    },
-  })
-  return post
-})
-
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany()
-  return users
-})
-
-app.get<{
-  Params: IPostByIdParam
-}>('/user/:id/drafts', async (req, res) => {
-  const { id } = req.params
-
-  const drafts = await prisma.user
-    .findUnique({
-      where: { id: Number(id) },
-    })
-    .posts({
-      where: { published: false },
-    })
-
-  return drafts
-})
-
-app.get<{
-  Params: IPostByIdParam
-}>(`/post/:id`, async (req, res) => {
-  const { id } = req.params
-
-  const post = await prisma.post.findUnique({
-    where: { id: Number(id) },
-  })
-  return post
-})
-
-app.get<{
-  Querystring: IFeedQueryString
-}>('/feed', async (req, res) => {
-  const { searchString, skip, take, orderBy } = req?.query
-
-  const or: Prisma.PostWhereInput = searchString
-    ? {
-      OR: [
-        { title: { contains: searchString as string } },
-        { content: { contains: searchString as string } },
-      ],
+    if (!user) {
+      reply.status(403).send({ message: "User not found" });
+      return;
     }
-    : {}
 
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
-      ...or,
-    },
-    include: { author: true },
-    take: Number(take) || undefined,
-    skip: Number(skip) || undefined,
-    orderBy: {
-      updatedAt: orderBy as Prisma.SortOrder,
-    },
-  })
-
-  return posts
-})
-interface IFeedQueryString {
-  searchString: string | null
-  skip: number | null
-  take: number | null
-  orderBy: Prisma.SortOrder | null
-}
-
-interface IPostByIdParam {
-  id: number
-}
-
-interface ICreatePostBody {
-  title: string
-  content: string | null
-  authorEmail: string
-}
-
-interface ISignupBody {
-  name: string | null
-  email: string
-  posts: Prisma.PostCreateInput[]
-}
-
-app.listen({ port: 3000 }, (err) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
+    // Attach the decoded user information to the request object
+    req.user = user;
+  } catch (e: unknown) {
+    reply.status(403).send({ message: "Invalid token" });
+    return;
   }
-  console.log(`
-  🚀 Server ready at: http://localhost:3000
-  ⭐️ See sample requests: http://pris.ly/e/ts/rest-fastify#3-using-the-rest-api`)
-})
+});
+
+app.post<{
+  Body: {
+    email: string;
+    username: string;
+    password: string;
+  };
+}>("/register", async (req, reply) => {
+  const { username, password, email } = req.body;
+
+  // Hash the password before saving
+  const encryptedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: encryptedPassword,
+    },
+  });
+
+  // PRODUCE: UserCreated
+
+  reply.send({ message: "User registered successfully" });
+});
+
+app.post<{
+  Body: {
+    email: string;
+    password: string;
+  };
+}>("/login", async (req, reply) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (!user) {
+    reply.status(401).send({ message: "Invalid email or password" });
+    return;
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    reply.status(401).send({ message: "Invalid email or password" });
+    return;
+  }
+
+  const token = jwt.sign({ userId: user.publicId }, SECRET_KEY);
+
+  reply.send({ token });
+});
+
+app.get("/users", async (req, reply) => {
+  if (req.user?.role !== "admin") {
+    reply.status(401).send({ message: "Not authorized" });
+    return;
+  }
+
+  const users = await prisma.user.findMany();
+
+  reply.status(200).send({ users });
+});
+
+app.put<{
+  Params: { id: number };
+  Body: { email: string; username: string; role: string };
+}>("/users/:id", async (req, reply) => {
+  if (req.user?.role !== "admin") {
+    reply.status(401).send({ message: "Not authorized" });
+    return;
+  }
+  const { id: _id } = req.params;
+
+  const id = Number(_id);
+
+  const { email, username, role } = req.body;
+
+  const oldUser = await prisma.user.findUnique({
+    where: { id: id },
+  });
+
+  if (!oldUser) {
+    reply.status(404).send({ message: "User not found" });
+    return;
+  }
+
+  const roleChanged = oldUser.role !== role;
+
+  const updatedUser = await prisma.user.update({
+    where: { id },
+    data: {
+      username,
+      role,
+      email,
+    },
+  });
+
+  // PRODUCE: UserUpdated
+
+  if (roleChanged) {
+    // PRODUCE: UserRoleChanged
+  }
+
+  reply.status(200).send({ updatedUser });
+});
+
+app.listen({ port: Number(PORT) }, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`🚀 Auth server ready at: http://localhost:${PORT}`);
+});
