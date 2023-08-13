279420273b76caa019069b136def3083cf7af07d
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2023-08-13 17:15:58 +0300

    W-2 Create Tasks Tracker basic functions

--- task-tracker/src/index.ts
+++ task-tracker/src/index.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import jwt from "jsonwebtoken";
 import { PrismaClient, User } from "@prisma/client";
+import { getRandomElement } from "./utils";
 
 declare module "fastify" {
   interface FastifyRequest {
@@ -45,6 +46,66 @@ app.addHook("preHandler", async (req, reply) => {
     reply.status(403).send({ message: "Invalid token" });
     return;
   }
+});
+
+app.get("/tasks", async (req, reply) => {
+  const tasks = await prisma.task.findMany();
+
+  reply.status(200).send({ tasks });
+});
+
+app.get<{ Params: { id: string } }>("/tasks/:id", async (req, reply) => {
+  const { id: _id } = req.params;
+
+  const id = Number(_id);
+
+  const task = await prisma.task.findUnique({ where: { id } });
+
+  if (!task) {
+    reply.status(404).send({ message: "Not found" });
+    return;
+  }
+
+  reply.status(200).send(task);
+});
+
+app.post<{
+  Body: {
+    title: string;
+    description: string;
+  };
+}>("/tasks", async (req, reply) => {
+  const possibleAssigneeIds = await prisma.user.findMany({
+    select: {
+      publicId: true,
+    },
+    where: {
+      NOT: {
+        role: {
+          in: ["admin", "manager"],
+        },
+      },
+    },
+  });
+
+  const { title, description } = req.body;
+
+  const assigneeId = getRandomElement(possibleAssigneeIds).publicId;
+
+  const task = await prisma.task.create({
+    data: {
+      title,
+      description,
+      userId: assigneeId,
+      status: "in-progress",
+    },
+  });
+
+  // PRODUCE: TaskCreated
+
+  // PRODUCE: TaskAssigned
+
+  reply.status(200).send(task);
 });
 
 app.listen({ port: Number(PORT) }, (err) => {
