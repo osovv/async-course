4181db0342655e800aadcca199405bf6bd0b10f5
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2023-08-13 19:30:32 +0300

    W-2 Add events

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,9 @@ import fastify from "fastify";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { PrismaClient, User } from "@prisma/client";
+import { Kafka } from "kafkajs";
+import { fastifyGracefulShutdown } from "fastify-graceful-shutdown";
+import { Message } from "kafkajs";
 
 declare module "fastify" {
   interface FastifyRequest {
@@ -12,152 +15,216 @@ declare module "fastify" {
 const SECRET_KEY = process.env.SECRET_KEY;
 const PORT = process.env.PORT;
 
-if (!SECRET_KEY || !PORT) {
-  throw new Error("Provide a SECRET_KEY and PORT env variables");
+if (!SECRET_KEY) {
+  throw new Error("Provide a SECRET_KEY env variable");
 }
 
-const prisma = new PrismaClient();
-const app = fastify({ logger: true }).decorate("user", null);
+if (!PORT) {
+  throw new Error("Provide a PORT env variable");
+}
+async function main({ secretKey, port }: { secretKey: string; port: number }) {
+  const kafka = new Kafka({
+    clientId: "auth",
+    brokers: ["localhost:9092"],
+  });
 
-app.addHook("preHandler", async (req, reply) => {
-  const token = req.headers.authorization?.replace("Bearer ", "");
+  const makeMessage = (event_name: string, data: unknown): Message => {
+    const event = {
+      event_name,
+      data,
+    };
 
-  if (!token) {
-    return;
-  }
+    return { value: JSON.stringify(event) };
+  };
 
-  try {
-    const decoded = jwt.verify(token, SECRET_KEY);
-    const validated = decoded as { userId: string };
+  const producer = kafka.producer();
+  await producer.connect();
+  const prisma = new PrismaClient();
 
-    const user = await prisma.user.findUnique({
-      where: { publicId: validated.userId },
+  const app = fastify({ logger: true }).decorate("user", null);
+  app.register(fastifyGracefulShutdown);
+
+  app.after(() => {
+    app.gracefulShutdown(async (signal, next) => {
+      console.log("Upps!");
+      await producer.disconnect();
+      next();
     });
+  });
 
-    if (!user) {
-      reply.status(403).send({ message: "User not found" });
+  app.addHook("preHandler", async (req, reply) => {
+    const token = req.headers.authorization?.replace("Bearer ", "");
+
+    if (!token) {
       return;
     }
 
-    // Attach the decoded user information to the request object
-    req.user = user;
-  } catch (e: unknown) {
-    reply.status(403).send({ message: "Invalid token" });
-    return;
-  }
-});
+    try {
+      const decoded = jwt.verify(token, secretKey);
+      const validated = decoded as { userId: string };
 
-app.post<{
-  Body: {
-    email: string;
-    username: string;
-    password: string;
-  };
-}>("/register", async (req, reply) => {
-  const { username, password, email } = req.body;
+      const user = await prisma.user.findUnique({
+        where: { publicId: validated.userId },
+      });
 
-  // Hash the password before saving
-  const encryptedPassword = await bcrypt.hash(password, 10);
+      if (!user) {
+        reply.status(403).send({ message: "User not found" });
+        return;
+      }
 
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: encryptedPassword,
-    },
+      // Attach the decoded user information to the request object
+      req.user = user;
+    } catch (e: unknown) {
+      reply.status(403).send({ message: "Invalid token" });
+      return;
+    }
   });
 
-  // PRODUCE: UserCreated
+  app.post<{
+    Body: {
+      email: string;
+      username: string;
+      password: string;
+    };
+  }>("/register", async (req, reply) => {
+    const { username, password, email } = req.body;
 
-  reply.send({ message: "User registered successfully" });
-});
+    // Hash the password before saving
+    const encryptedPassword = await bcrypt.hash(password, 10);
 
-app.post<{
-  Body: {
-    email: string;
-    password: string;
-  };
-}>("/login", async (req, reply) => {
-  const { email, password } = req.body;
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: encryptedPassword,
+      },
+    });
 
-  const user = await prisma.user.findUnique({
-    where: { email },
+    await producer.send({
+      topic: "users-stream",
+      messages: [
+        makeMessage("user_created", {
+          username,
+          email,
+          publicId: user.publicId,
+          role: user.role,
+          createdAt: user.createdAt,
+          updatedAt: user.updatedAt,
+          active: true,
+        }),
+      ],
+    });
+
+    reply.send({ message: "User registered successfully" });
   });
 
-  if (!user) {
-    reply.status(401).send({ message: "Invalid email or password" });
-    return;
-  }
+  app.post<{
+    Body: {
+      email: string;
+      password: string;
+    };
+  }>("/login", async (req, reply) => {
+    const { email, password } = req.body;
 
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) {
-    reply.status(401).send({ message: "Invalid email or password" });
-    return;
-  }
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
 
-  const token = jwt.sign({ userId: user.publicId }, SECRET_KEY);
+    if (!user) {
+      reply.status(401).send({ message: "Invalid email or password" });
+      return;
+    }
 
-  reply.send({ token });
-});
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      reply.status(401).send({ message: "Invalid email or password" });
+      return;
+    }
 
-app.get("/users", async (req, reply) => {
-  if (req.user?.role !== "admin") {
-    reply.status(401).send({ message: "Not authorized" });
-    return;
-  }
+    const token = jwt.sign({ userId: user.publicId }, secretKey);
 
-  const users = await prisma.user.findMany();
-
-  reply.status(200).send({ users });
-});
-
-app.put<{
-  Params: { id: number };
-  Body: { email: string; username: string; role: string };
-}>("/users/:id", async (req, reply) => {
-  if (req.user?.role !== "admin") {
-    reply.status(401).send({ message: "Not authorized" });
-    return;
-  }
-  const { id: _id } = req.params;
-
-  const id = Number(_id);
-
-  const { email, username, role } = req.body;
-
-  const oldUser = await prisma.user.findUnique({
-    where: { id: id },
+    reply.send({ token });
   });
 
-  if (!oldUser) {
-    reply.status(404).send({ message: "User not found" });
-    return;
-  }
+  app.get("/users", async (req, reply) => {
+    if (req.user?.role !== "admin") {
+      reply.status(401).send({ message: "Not authorized" });
+      return;
+    }
 
-  const roleChanged = oldUser.role !== role;
+    const users = await prisma.user.findMany();
 
-  const updatedUser = await prisma.user.update({
-    where: { id },
-    data: {
-      username,
-      role,
-      email,
-    },
+    reply.status(200).send({ users });
   });
 
-  // PRODUCE: UserUpdated
+  app.put<{
+    Params: { id: number };
+    Body: { email: string; username: string; role: string };
+  }>("/users/:id", async (req, reply) => {
+    if (req.user?.role !== "admin") {
+      reply.status(401).send({ message: "Not authorized" });
+      return;
+    }
+    const { id: _id } = req.params;
 
-  if (roleChanged) {
-    // PRODUCE: UserRoleChanged
-  }
+    const id = Number(_id);
 
-  reply.status(200).send({ updatedUser });
-});
+    const { email, username, role } = req.body;
 
-app.listen({ port: Number(PORT) }, (err) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`🚀 Auth server ready at: http://localhost:${PORT}`);
-});
+    const oldUser = await prisma.user.findUnique({
+      where: { id: id },
+    });
+
+    if (!oldUser) {
+      reply.status(404).send({ message: "User not found" });
+      return;
+    }
+
+    const roleChanged = oldUser.role !== role;
+
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: {
+        username,
+        role,
+        email,
+      },
+    });
+
+    await producer.send({
+      topic: "users-stream",
+      messages: [
+        makeMessage("user_updated", {
+          username,
+          email,
+          publicId: updatedUser.publicId,
+          updatedAt: updatedUser.updatedAt,
+        }),
+      ],
+    });
+
+    if (roleChanged) {
+      await producer.send({
+        topic: "users",
+        messages: [
+          makeMessage("user_role_changed", {
+            role: updatedUser.role,
+            publicId: updatedUser.publicId,
+          }),
+        ],
+      });
+    }
+
+    reply.status(200).send({ updatedUser });
+  });
+
+  app.listen({ port }, (err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`🚀 Auth server ready at: http://localhost:${PORT}`);
+  });
+}
+
+main({ secretKey: SECRET_KEY, port: Number(PORT) });
--- task-tracker/src/index.ts
+++ task-tracker/src/index.ts
@@ -2,6 +2,8 @@ import fastify from "fastify";
 import jwt from "jsonwebtoken";
 import { PrismaClient, User } from "@prisma/client";
 import { getRandomElement } from "./utils";
+import { fastifyGracefulShutdown } from "fastify-graceful-shutdown";
+import { Kafka, Message } from "kafkajs";
 
 declare module "fastify" {
   interface FastifyRequest {
@@ -15,103 +17,250 @@ const PORT = process.env.PORT;
 if (!SECRET_KEY || !PORT) {
   throw new Error("Provide a SECRET_KEY and PORT env variables");
 }
+async function main({ secretKey, port }: { secretKey: string; port: number }) {
+  const kafka = new Kafka({
+    clientId: "task-tracker",
+    brokers: ["localhost:9092"],
+  });
 
-const prisma = new PrismaClient();
-const app = fastify({ logger: true }).decorate("user", null);
+  const makeMessage = (event_name: string, data: unknown): Message => {
+    const event = {
+      event_name,
+      data,
+    };
 
-app.addHook("preHandler", async (req, reply) => {
-  const token = req.headers.authorization?.replace("Bearer ", "");
+    return { value: JSON.stringify(event) };
+  };
 
-  if (!token) {
-    reply.status(403).send({ message: "No token present!" });
-    return;
-  }
+  const prisma = new PrismaClient();
+  const app = fastify({ logger: true }).decorate("user", null);
+  app.register(fastifyGracefulShutdown);
 
-  try {
-    const decoded = jwt.verify(token, SECRET_KEY);
-    const validated = decoded as { userId: string };
+  const consumer = kafka.consumer({ groupId: "task-tracker-group" });
+  await consumer.connect();
 
-    const user = await prisma.user.findUnique({
-      where: { publicId: validated.userId },
+  const producer = kafka.producer();
+  await producer.connect();
+
+  app.after(() => {
+    app.gracefulShutdown(async (signal, next) => {
+      console.log("Upps!");
+      await consumer.disconnect();
+      await producer.disconnect();
+      next();
     });
+  });
 
-    if (!user) {
-      reply.status(403).send({ message: "User not found" });
+  await consumer.subscribe({ topics: ["users-stream", "users"] });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
+      if (message.value === null) {
+        console.error("empty event");
+        return;
+      }
+
+      const content = message?.value.toString();
+      const event = JSON.parse(content);
+
+      console.log("NEW MESSAGE ARRIVED", topic, event);
+
+      if (topic === "users-stream") {
+        switch (event?.event_name) {
+          case "user_created":
+            const user = await prisma.user.create({ data: event.data });
+            console.log("CREATED USER", user);
+            break;
+          case "user_updated":
+            await prisma.user.update({
+              where: { publicId: event?.data.publicId },
+              data: {
+                email: event?.data.email,
+                username: event?.data.username,
+                updatedAt: event?.data.updatedAt,
+              },
+            });
+            break;
+          default:
+            console.warn(`Unknown users-stream event: ${event?.event_name}`);
+        }
+      }
+
+      if (topic === "users") {
+        switch (event?.event_name) {
+          case "user_role_changed":
+            const user = await prisma.user.update({
+              where: { publicId: event.data.publicId },
+              data: {
+                role: event.data.role,
+              },
+            });
+            break;
+          default:
+            console.warn(`Unknown users-stream event: ${event?.event_name}`);
+        }
+      }
+    },
+  });
+
+  app.addHook("preHandler", async (req, reply) => {
+    const token = req.headers.authorization?.replace("Bearer ", "");
+
+    if (!token) {
+      reply.status(403).send({ message: "No token present!" });
       return;
     }
 
-    // Attach the decoded user information to the request object
-    req.user = user;
-  } catch (e: unknown) {
-    reply.status(403).send({ message: "Invalid token" });
-    return;
-  }
-});
+    try {
+      const decoded = jwt.verify(token, secretKey);
+      const validated = decoded as { userId: string };
 
-app.get("/tasks", async (req, reply) => {
-  const tasks = await prisma.task.findMany();
+      const user = await prisma.user.findUnique({
+        where: { publicId: validated.userId },
+      });
 
-  reply.status(200).send({ tasks });
-});
+      if (!user) {
+        reply.status(403).send({ message: "User not found" });
+        return;
+      }
 
-app.get<{ Params: { id: string } }>("/tasks/:id", async (req, reply) => {
-  const { id: _id } = req.params;
+      // Attach the decoded user information to the request object
+      req.user = user;
+    } catch (e: unknown) {
+      reply.status(403).send({ message: "Invalid token" });
+      return;
+    }
+  });
 
-  const id = Number(_id);
+  app.get("/tasks", async (req, reply) => {
+    const tasks = await prisma.task.findMany();
 
-  const task = await prisma.task.findUnique({ where: { id } });
+    reply.status(200).send({ tasks });
+  });
 
-  if (!task) {
-    reply.status(404).send({ message: "Not found" });
-    return;
-  }
+  app.get<{ Params: { id: string } }>("/tasks/:id", async (req, reply) => {
+    const { id: _id } = req.params;
 
-  reply.status(200).send(task);
-});
+    const id = Number(_id);
 
-app.post<{
-  Body: {
-    title: string;
-    description: string;
-  };
-}>("/tasks", async (req, reply) => {
-  const possibleAssigneeIds = await prisma.user.findMany({
-    select: {
-      publicId: true,
-    },
-    where: {
-      NOT: {
-        role: {
-          in: ["admin", "manager"],
+    const task = await prisma.task.findUnique({ where: { id } });
+
+    if (!task) {
+      reply.status(404).send({ message: "Not found" });
+      return;
+    }
+
+    reply.status(200).send(task);
+  });
+
+  app.patch<{
+    Params: { id: string };
+    Body: { status?: "completed"; title?: string; description?: string };
+  }>("/tasks/:id", async (req, reply) => {
+    const { id: _id } = req.params;
+    const id = Number(_id);
+    const { status, title, description } = req.body;
+    const patch = { status, title, description };
+
+    const oldTask = await prisma.task.findUnique({
+      where: { id: id },
+    });
+
+    if (!oldTask) {
+      reply.status(404).send({ message: "Task not found" });
+      return;
+    }
+
+    const updatedTask = await prisma.task.update({
+      where: { id },
+      data: patch,
+    });
+
+    const taskCompleted =
+      oldTask.status === "in-progress" && updatedTask.status === "completed";
+
+    if (taskCompleted) {
+      await producer.send({
+        topic: "tasks",
+        messages: [
+          makeMessage("task_completed", {
+            publicId: updatedTask.publicId,
+            assigneeId: updatedTask.userId,
+          }),
+        ],
+      });
+    }
+  });
+
+  app.post<{
+    Body: {
+      title: string;
+      description: string;
+    };
+  }>("/tasks", async (req, reply) => {
+    const possibleAssigneeIds = await prisma.user.findMany({
+      select: {
+        publicId: true,
+      },
+      where: {
+        NOT: {
+          role: {
+            in: ["admin", "manager"],
+          },
         },
       },
-    },
+    });
+
+    const { title, description } = req.body;
+
+    const assigneeId = getRandomElement(possibleAssigneeIds).publicId;
+
+    const task = await prisma.task.create({
+      data: {
+        title,
+        description,
+        userId: assigneeId,
+        status: "in-progress",
+      },
+    });
+
+    // PRODUCE: TaskCreated
+
+    await producer.send({
+      topic: "tasks",
+      messages: [
+        makeMessage("task_created", {
+          title: task.title,
+          description: task.description,
+          publicId: task.publicId,
+          status: task.status,
+          assigneeId: task.userId,
+        }),
+      ],
+    });
+
+    // PRODUCE: TaskAssigned
+
+    await producer.send({
+      topic: "tasks",
+      messages: [
+        makeMessage("task_assigned", {
+          publicId: task.publicId,
+          assigneeId: task.userId,
+        }),
+      ],
+    });
+
+    reply.status(200).send(task);
   });
 
-  const { title, description } = req.body;
-
-  const assigneeId = getRandomElement(possibleAssigneeIds).publicId;
-
-  const task = await prisma.task.create({
-    data: {
-      title,
-      description,
-      userId: assigneeId,
-      status: "in-progress",
-    },
+  app.listen({ port }, (err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`🚀 Task Tracker server ready at: http://localhost:${PORT}`);
   });
+}
 
-  // PRODUCE: TaskCreated
-
-  // PRODUCE: TaskAssigned
-
-  reply.status(200).send(task);
-});
-
-app.listen({ port: Number(PORT) }, (err) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`🚀 Task Tracker server ready at: http://localhost:${PORT}`);
-});
+main({ secretKey: SECRET_KEY, port: Number(PORT) });
